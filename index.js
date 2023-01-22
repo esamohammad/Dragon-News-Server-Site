f771c7d513930628cc5a67c0f6f5abd5fddb3f95
mdesabd <esa.ce2020@gmail.com>
2023-01-23 00:11:37 +0600

    cors named

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
+const cors = require('cors'); //middle ware
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors());  // middele ware 
 
 //data send --
 const categories = require('./data/category.json');
