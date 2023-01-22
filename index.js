01d05f81bb875fa0f89f18e1929f61ad108ab243
mdesabd <esa.ce2020@gmail.com>
2023-01-23 05:19:12 +0600

    Create single News and category news data loading API

--- index.js
+++ index.js
@@ -5,19 +5,79 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());  // middele ware 
 
-//data send --
+
+
+//💥💥💥💥💥💥💥💥💥💥💥💥💥
+//data send --ui***
 const categories = require('./data/category.json');
+//data send --ui@@@
+const news = require('./data/news.json');
+//💥💥💥💥💥💥💥💥💥💥💥💥💥
 
 
+
+
+//default
 app.get('/', (req, res) => {
-  res.send('News Api Running') //ata Ui te dekhabe
+  res.send('News Api Running') 
+  //ata Ui te dekhabe
 });
 
+
+
+
+
+//💥💥💥💥💥💥💥💥💥💥💥💥💥
+//data send --ui***
 app.get('/news-categories', (req, res) => {
   res.send(categories)
 });
+//💥💥💥💥💥💥💥💥💥💥💥💥💥
 
 
+
+
+
+
+//💥💥💥💥💥💥💥💥💥💥💥💥💥
+//category wise data
+app.get('/category/:id', (req, res) => {
+  const id = req.params.id;
+  if (id === '08') {
+      res.send(news);
+  }
+  else {
+      const category_news = news.filter(n => n.category_id === id);
+      res.send(category_news);
+  }
+})
+//💥💥💥💥💥💥💥💥💥💥💥💥💥
+
+
+
+
+
+
+
+
+
+//💥💥💥💥💥💥💥💥💥💥💥💥💥
+//params--- only for one news item call single news@@@
+app.get('/news/:id', (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find(n => n._id === id); 
+  //*news ke find */
+  res.send(selectedNews);
+});
+//💥💥💥💥💥💥💥💥💥💥💥💥💥
+
+
+
+
+
+
+
+//default
 app.listen(port, () => {
   console.log(`Dragon News Server Running on port ${port}`)
 });
