52cf5db4b59525f201d9832840cdaec43d71c6dd
mdesabd <esa.ce2020@gmail.com>
2023-01-22 23:32:35 +0600

    data get done and check it see pic

--- index.js
+++ index.js
@@ -2,13 +2,23 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
 
+
+//data send --
+const categories = require('./data/category.json');
+
+
 app.get('/', (req, res) => {
   res.send('News Api Running') //ata Ui te dekhabe
-})
+});
+
+app.get('/news-categories', (req, res) => {
+  res.send(categories)
+});
+
 
 app.listen(port, () => {
   console.log(`Dragon News Server Running on port ${port}`)
-})
+});
 
 //ata cmd server a dekhabe
 // Dragon News Server Running on port
