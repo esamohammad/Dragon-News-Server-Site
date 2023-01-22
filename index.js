5994a22f8a7355e9caaa41edcd26cad58c900427
mdesabd <esa.ce2020@gmail.com>
2023-01-23 00:10:13 +0600

    cors policy done it is mandatory for interconnection of different port

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
+app.use(cors());
 
 //data send --
 const categories = require('./data/category.json');
